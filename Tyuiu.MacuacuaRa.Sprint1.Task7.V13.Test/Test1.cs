9d88f4cc1788d0d07a07846da34f56bb4cc833a6
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 01:45:25 +0500

    Task7

---
+++ Tyuiu.MacuacuaRa.Sprint1.Task7.V13.Test/Test1.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Tyuiu.MacuacuaRa.Sprint1.Task7.V13.Lib;
+namespace Tyuiu.MacuacuaRa.Sprint1.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service1 = new DataService();
+            double x = 45;
+            double y = 30;
+            var res = service1.Calculate(x, y);
+            Assert.AreEqual(1, 001, res);
+        }
+    }
+}
---
+++ Tyuiu.MacuacuaRa.Sprint1.Task7.V13/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Tyuiu.MacuacuaRa.Sprint1.Task7.V13.Lib;
+namespace Tyuiu.MacuacuaRa.Sprint1.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите x=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y=");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("   y^2+cos(x^2)+10");
+            Console.WriteLine("z=-----------------=" + ds.Calculate(x, y));
+            Console.WriteLine("   x^2+sin(y^2)+10");
+
+            Console.ReadKey();
+        }
+    }
+}
