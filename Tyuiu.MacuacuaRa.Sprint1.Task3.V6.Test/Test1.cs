d2d9f53867d8b94e5565a336634df0e2903c86e4
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 01:09:58 +0500

    Task3

---
+++ Tyuiu.MacuacuaRa.Sprint1.Task3.V6.Test/Test1.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MacuacuaRa.Sprint1.Task3.V6.Lib;
+
+namespace Tyuiu.MacuacuaRa.Sprint1.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double distance = 67.0;
+            double gasFlow = 8.5;
+            double gasPrice = 6.5;
+
+            var res = ds.TravelCost(distance, gasFlow, gasPrice);
+            Assert.AreEqual(74.035, res);
+        }
+    }
+}
---
+++ Tyuiu.MacuacuaRa.Sprint1.Task3.V6/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.MacuacuaRa.Sprint1.Task3.V6.Lib;
+namespace Tyuiu.MacuacuaRa.Sprint1.Task3.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            double distance = 67.0;
+            double gasFlow = 8.5;
+            double gasPrice = 6.5;
+            Console.WriteLine("Расстояние до дачи(км) = " + distance);
+            Console.WriteLine("Расход бензина (литров на 100 км пробега) = " + gasFlow);
+            Console.WriteLine("Цена литра бензина (руб.) = " + gasPrice);
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Поездка на дачу и обратно обойдется в " + ds.TravelCost(distance, gasFlow, gasPrice) + "руб.");
+            Console.ReadLine();
+        }
+    }
+}
