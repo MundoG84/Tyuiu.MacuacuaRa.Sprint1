e12134dfb743ced273b8078cb4da65e0211b4080
MundoG84 <raimundomacuacua271121@gmail.com>
2024-12-28 01:27:01 +0500

    Task5

---
+++ Tyuiu.MacuacuaRa.Sprint1.Task5.V6.Test/Test1.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MacuacuaRa.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.MacuacuaRa.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int k = 20;
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(k);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 6;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.MacuacuaRa.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MacuacuaRa.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.MacuacuaRa.Sprint1.Task5.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Введите день года: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = Convert.ToInt32(ds.Calculate(k));
+            Console.WriteLine("День недели: " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
